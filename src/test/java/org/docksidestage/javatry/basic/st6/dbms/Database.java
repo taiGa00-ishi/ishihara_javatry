12896ce57b399a17d132aaadd536ed660a1f4af1
taiGa00-ishi <taiga.ishihara@bizreach.co.jp>
2025-10-17 16:50:10 +0900

    try: 8週目のJavaTryコミット javadoc追記

--- src/test/java/org/docksidestage/javatry/basic/st6/dbms/Database.java
+++ src/test/java/org/docksidestage/javatry/basic/st6/dbms/Database.java
@@ -1,5 +1,12 @@
 package org.docksidestage.javatry.basic.st6.dbms;
 
+/**
+ * データベースを表す抽象クラスです。
+ * <p>
+ * このクラスは、異なるデータベースシステムに対応するための共通のメソッドを提供する。
+ * </p>
+ * @author taiGa00-ishi
+ */
 public abstract class Database {
 
     public abstract String buildPagingQuery(int pageSize, int pageNumber);
