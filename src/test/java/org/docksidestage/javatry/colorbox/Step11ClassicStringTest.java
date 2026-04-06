996e605bc6adad16fa327fb49c35f5e89dbfb6d7
jflute <jflute@localhost>
2026-04-06 18:07:34 +0900

    1on1ふぉろー

--- src/test/java/org/docksidestage/javatry/colorbox/Step11ClassicStringTest.java
+++ src/test/java/org/docksidestage/javatry/colorbox/Step11ClassicStringTest.java
@@ -62,9 +62,12 @@ public class Step11ClassicStringTest extends PlainTestCase {
      */
     public void test_length_findMax_colorSize() {
         List<ColorBox> colorBoxList = new YourPrivateRoom().getColorBoxList();
+        // TODO ishihara not found のときに log("*not found") 的なものが出るようにお願いします by jflute (2026/04/06)
         if (!colorBoxList.isEmpty()) {
             int max = 0;
             String maxColorName = "";
+
+            // #1on1: ここはあえて基本 int iループを使ってみたということ // (2026/04/06)
             for (int i = 0; i < colorBoxList.size(); i++) {
                 ColorBox colorBox = colorBoxList.get(i);
                 BoxColor boxColor = colorBox.getColor();
@@ -85,6 +88,10 @@ public class Step11ClassicStringTest extends PlainTestCase {
     public void test_length_findMax_stringContent() {
         List<ColorBox> colorBoxList = new YourPrivateRoom().getColorBoxList();
         if (!colorBoxList.isEmpty()) {
+            // TODO ishihara longestWordとしてるくらいなら、longestLengthが嬉しい by jflute (2026/04/06)
+            // この二つの変数は関連性が強く、連動するもの、であることをぱっと見でわかりたい。
+            // 一方で、スコープ短いからすっきり変数にする方向性であれば、逆にwordでいいのかなと。
+            // (一方で一方で、jfluteの好みだと、maxとcurrentのlengthを区別したい)
             int length = 0;
             String longestWord = "";
             for (ColorBox colorBox : colorBoxList) {
@@ -105,6 +112,37 @@ public class Step11ClassicStringTest extends PlainTestCase {
 
     // どうしてもfor分だとインデントたくさんあるような書き方になってしまう。
     // 他にスッキリかける方法はあるのだろうか？
+    //
+    // #1on1: ifのところは、判定を逆転させて、continueすることでインデントを減らせるが... (2026/04/06)
+    // あまり論理が直感的ではなくなるかもなので本末転倒感もある。
+    // そういう意味ではインデント自体は悪く無く、人間に直感性を与えるための道具ではある。
+    // ただ、あまりに多いと全体像が把握しづらくなるのかも。
+    //
+    // ループの方は、Stream APIをつかわない前提であれば、なかなか二重ループを無くすのは難しく...
+    // 事前にallSpaceListとか作っておけば減らせなくはないけど、わざわざそれをやるか？ってのもある。
+    // あと、privateメソッド切り出しで色々チャレンジしてみたけど、どれも微妙。
+    // ２つのローカル変数で更新しないといけないので、複数の戻り値を戻す入れ物クラスとか必要になっちゃう。
+    //
+    // ただ、longestWord だけで制御できなくはないので、更新変数が一個だけなら↑のやり方も採用できるかも。
+    // if (str.length() > longestWord.length()) {
+    // length() は、value.length してるだけなので何度も読んでもまあパフォーマンスも全然気にならないし。
+    //
+    // 中でのif文。
+    /*
+if (length > 0) {
+    log(length + " (" + longestWord + ")");
+} else {
+    log("*not found");
+}
+     */
+    // maxLength=0 で初期化していると、contentの中、空文字だけが見つかった場合、
+    // それが区別できなくなってしまう。(区別が必要かどうかはそのときの業務次第)
+    // jfluteの個人的なポリシーだと、わりとnull初期化を使う。まだ探してないからないものはない。
+    // -1とかだと、MINを求める時使えないとか、ある程度そのデフォルト値が妥当かどうか気にしないといけない。
+    //
+    // 少し余談、nullと空文字の混在のお話。
+    // xxxxxxx?productName=&productStatus=...
+    // DBのカラムに混在するとちょっとやっかい。
 
     /**
      * Which value (toString() if non-string) has second-max length in color-boxes? (latter if same length) <br>
